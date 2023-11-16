c7ebe8c4986380fc175a3d41c5de6bfa1ea96a98
Сосидка Илья <isosidka@stsoft.ru>
2023-11-16 11:25:08 +0300

    fix: 2.x build fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,30 @@ import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { version } from 'vue'
+import { parse } from 'semver'
+import AutoImport from 'unplugin-auto-import/vite'
+const { major, minor } = parse(version)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        major === 3 || (major === 2 && minor >= 7) ? 'vue' : '@vue/composition-api'
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -22,9 +42,10 @@ export default defineConfig({
       name: 'bootsman-ui'
     },
     rollupOptions: {
-      external: ['vue-demi'],
+      external: ['vue-demi', 'vue'],
       output: {
         globals: {
+          vue: 'Vue',
           'vue-demi': 'VueDemi'
         }
       }
