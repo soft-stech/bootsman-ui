47809e5f357db3089277cb6e75b6c4687ccee904
Victoria Karpova <121668368+victoriakaropva@users.noreply.github.com>
2023-11-24 11:58:48 +0300

    feat: Checkbox element (refs #DH-831) (#24)

---
+++ src/components/BuiCheckbox/BuiCheckbox.story.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import BuiCheckbox from './BuiCheckbox.vue'
+import { ref } from 'vue'
+
+const model = ref(false)
+const model2 = ref(false)
+</script>
+
+<template>
+  <Story title="BuiCheckbox" :layout="{ type: 'grid', width: '400px' }">
+    <Variant title="INFO">
+      {{ `v-model value: ${model}` }}
+      <br />
+      {{ `v-model option 2 value: ${model2}` }}
+    </Variant>
+    <Variant title="Only checkbox">
+      <div class="p-2">
+        <BuiCheckbox v-model="model" />
+      </div>
+    </Variant>
+    <Variant title="Default">
+      <div class="p-2">
+        <BuiCheckbox v-model="model">
+          My Label
+          <template #description>My description</template>
+        </BuiCheckbox>
+      </div>
+    </Variant>
+    <Variant title="Disabled">
+      <div class="p-2">
+        <BuiCheckbox :disabled="true" v-model="model">
+          My Label
+          <template #description>My description</template>
+        </BuiCheckbox>
+      </div>
+    </Variant>
+    <Variant title="List">
+      <form ref="modelGeneral">
+        <div class="flex flex-col">
+          <BuiCheckbox groupName="options" option-name="option1" v-model="model"
+            >Option 1
+          </BuiCheckbox>
+          <BuiCheckbox groupName="options" option-name="option2" v-model="model2"
+            >Option 2</BuiCheckbox
+          >
+        </div>
+      </form>
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/BuiCheckbox/svgComponents/InactiveCheckboxIcon.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+    <rect x="0.5" y="0.5" width="15" height="15" rx="1.5" :class="rectClasses" />
+  </svg>
+</template>
+
+<style scoped>
+.bui-checkbox:not(.disabled) {
+  fill-opacity: 0.16;
+  stroke-opacity: 1;
+}
+.group:hover .bui-checkbox {
+  fill-opacity: 1;
+  stroke-opacity: 1;
+}
+.dark .group:hover .bui-checkbox {
+  fill-opacity: 0.32;
+  stroke-opacity: 1;
+}
+
+.dark .bui-checkbox.disabled {
+  fill-opacity: 0.16 !important;
+  stroke-opacity: 0.16 !important;
+}
+</style>
+
+<script setup lang="ts">
+import { twMerge } from 'tailwind-merge'
+
+const props = withDefaults(defineProps<{ disabled: boolean }>(), {
+  disabled: false
+})
+
+const rectClasses = twMerge(
+  'bui-checkbox',
+  'dark:stroke-primary-500 dark:fill-primary-500  dark:group-hover:fill-primary-500 dark:group-hover:[.bui-checkbox-hover]',
+  'cursor-pointer stroke-gray-300 fill-white group-hover:fill-gray-150',
+  props.disabled &&
+    'disabled cursor-default dark:fill-white dark:stroke-white dark:group-hover:fill-white fill-slate-200 group-hover:fill-slate-200'
+)
+</script>
