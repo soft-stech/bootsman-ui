9ddd66cd2a3c0e251844e2fbe4d26b2fb7af9be8
Victoria Karpova <121668368+victoriakaropva@users.noreply.github.com>
2023-11-27 12:26:08 +0300

    feat: Collapsable section element (refs #DH-899) (#26)

---
+++ src/components/BuiCollapsableSection/svgComponents/ArrowButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    class="cursor-pointer"
+  >
+    <g class="opacity-[0.56] dark:opacity-[1]">
+      <rect width="16" height="16" rx="4" fill="#7371F9" />
+      <path
+        d="M12 10L8 6L4 10"
+        stroke="#F5F6F8"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+  </svg>
+</template>
