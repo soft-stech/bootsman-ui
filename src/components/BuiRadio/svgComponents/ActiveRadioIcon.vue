5b3111439d95555713e42e3cb1bbc4845e9c1f23
Виктория Григорьевна Карпова <vkarpova@stsoft.ru>
2023-11-28 14:57:29 +0300

    fix: Accordion, Checkbox, Radio, Toggle changes

--- src/components/BuiRadio/svgComponents/ActiveRadioIcon.vue
+++ src/components/BuiRadio/svgComponents/ActiveRadioIcon.vue
@@ -6,13 +6,15 @@
 </template>
 <script setup lang="ts">
 import { twMerge } from 'tailwind-merge'
-const props = withDefaults(defineProps<{ disabled: boolean }>(), {
-  disabled: false
+const props = withDefaults(defineProps<{ disabled: boolean; readonly: boolean }>(), {
+  disabled: false,
+  readonly: false
 })
 
 const rectClasses = twMerge(
   'cursor-pointer stroke-primary-500 group-hover:stroke-primary-550  fill-white group-hover:fill-slate-150',
   props.disabled &&
-    'cursor-default group-hover:stroke-primary-500 group-hover:fill-transparent opacity-[0.32] fill-transparent'
+    'cursor-not-allowed group-hover:stroke-primary-500 group-hover:fill-transparent opacity-[0.32] fill-transparent',
+  props.readonly && 'cursor-default group-hover:stroke-primary-500 group-hover:fill-white'
 )
 </script>
