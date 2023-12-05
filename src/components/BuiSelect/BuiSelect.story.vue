ba58e9830acfae1b0b9496555e1c2867bcca17cb
Victoria Karpova <121668368+victoriakaropva@users.noreply.github.com>
2023-12-05 17:00:41 +0300

    feat: Added validation for Select (#33)
    * feat: Added validation for Select
    
    * fix: hide validation message on null status, build error

--- src/components/BuiSelect/BuiSelect.story.vue
+++ src/components/BuiSelect/BuiSelect.story.vue
@@ -3,7 +3,7 @@ import BuiSelect from './BuiSelect.vue'
 import BuiInput from '../BuiInput/BuiInput.vue'
 import { ref } from 'vue'
 
-const model = ref(null)
+const model = ref('')
 const options = [
   {
     name: 'One',
@@ -20,7 +20,18 @@ const options = [
   <Story title="BuiSelect" autoPropsDisabled :layout="{ type: 'grid', width: '400px' }">
     <Variant title="Default">
       <div class="p-2">
-        <BuiSelect label="Default" v-model="model" :options="options" :required="true" />
+        <BuiSelect label="Default" v-model="model" :options="options" />
+      </div>
+    </Variant>
+    <Variant title="Required">
+      <div class="p-2">
+        <BuiSelect
+          label="Default"
+          v-model="model"
+          :options="options"
+          :required="true"
+          :validation-status="model ? null : 'error'"
+        />
       </div>
     </Variant>
     <Variant title="Disabled">
@@ -33,6 +44,38 @@ const options = [
         <BuiSelect label="Default" v-model="model" :required="true" :options="options" />
         <BuiInput label="Default" :required="true" />
       </div>
+    </Variant>
+    <Variant title="With error status forced">
+      <div class="p-2">
+        <BuiSelect
+          label="Default"
+          :options="options"
+          :validation-status="'error'"
+          :has-forced-validation="true"
+        >
+          <template #validationMessage> Some error message </template>
+        </BuiSelect>
+      </div>
+    </Variant>
+    <Variant title="With success status forced">
+      <BuiSelect
+        label="Default"
+        :options="options"
+        :validation-status="'success'"
+        :has-forced-validation="true"
+      >
+        <template #validationMessage> Some success message </template>
+      </BuiSelect>
+    </Variant>
+    <Variant title="Required with dynamic validation">
+      <BuiSelect
+        label="Select right option (2)"
+        :options="options"
+        v-model="model"
+        :validation-status="model !== '1' ? null : 'error'"
+      >
+        <template #validationMessage> Do not select 'One'</template>
+      </BuiSelect>
     </Variant>
   </Story>
 </template>
